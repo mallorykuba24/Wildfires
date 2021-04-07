cb3e8c477411fb3eb638b88cdadbf70431a4f64d
Mallory Kuba <mallory.kuba@gmail.com>
2021-04-06 23:05:22 -0400

    ok

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask_pymongo import PyMongo
+from flask import render_template, redirect, Flask, jsonify
+
+app = Flask(__name__)
+
+mongo = PyMongo(app, uri='mongodb://localhost:27017/wildfires_db')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
